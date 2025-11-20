a845406ecdf7d8f62b6d1e2b128a70c766ae5f7f
Matheus Braga <sbmatheus20@gmail.com>
2025-11-20 18:59:14 -0300

    Delete scripts.js

--- projeto/scripts.js
+++
@@ -1,6 +0,0 @@
-function gerarNumber() {
-    const min = parseInt(document.getElementById('min').value);
-    const max = parseInt(document.getElementById('max').value);
-    const resultado = Math.floor(Math.random() * (max - min + 1)) + min;
-    document.getElementById('resultado').innerText = `Número sorteado: ${resultado}`;
-}
